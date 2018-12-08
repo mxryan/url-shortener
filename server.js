166b943ee3a2ccb29a529c3515aa68803d8a2b99
M Ryan <mxryan@live.com>
2018-12-08 16:02:48 -0500

    added front end fetch for api/shorturl/new

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const app = express();
 const dns = require("dns");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 const shortURLs = [];
-const originalURLs = [];
+const originalURLs = ["freecodecamp.org"];
 const genericErrMsg = {
   error: "You hit a route but something went wrong"
 }
 
 // to do:
 // 0 still does not work as a shorturl
+// eliminate extra URL array
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join("public")));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hi</h1>");
+  res.sendFile("public/index.html")
 });
 
 app.post("/api/shorturl/new", (req, res) => {
+  console.log("Hit post route");
   dns.lookup(req.body.url, (err, address) => {
     if (err) {
       res.json({
@@ -29,25 +33,27 @@ app.post("/api/shorturl/new", (req, res) => {
       shortURLs.push(address);
       originalURLs.push(req.body.url);
       res.json({
-        shortURL: originalURLs.length - 1,
-        originalURL: req.body.url
+        short_url: originalURLs.length - 1,
+        original_url: req.body.url
       });
     }
   })
 });
 
+
 app.get("/api/shorturl/:arg", (req, res) => {
-  console.log(shortURLs);
+  console.log(originalURLs);
   if (parseInt(req.params.arg)) {
-    if (shortURLs[parseInt(req.params.arg)]) {
-      
-      console.log(res)
-      
+    console.log("outer if");
+    if (originalURLs[parseInt(req.params.arg)]) {
+      console.log("inner if");
       res.redirect("http://"+ originalURLs[parseInt(req.params.arg)])
     } else {
+      console.log("inner else");
       res.json(genericErrMsg);
     }
   } else {
+    console.log("outer else");
     res.json(genericErrMsg)
   }
 })
